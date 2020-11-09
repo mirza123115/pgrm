465011695b3485f5905ed051a730e55f2fd811d9
emon <60744237+mirza123115@users.noreply.github.com>
2020-11-09 16:55:17 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+   int x=20;
+  // int b=44;
+main()
+
+{
+
+   int a,b,x;
+
+   printf("first number a :");
+   scanf("%d",&a);
+   printf("second number b :");
+   scanf("%d",&b);
+
+
+
+   if(a==1);
+
+      {
+
+
+       printf("%d is a positive nmbr",x);
+
+
+      }
+    if();
+    {
+        sub=a-b;
+        printf ("subtraction is %d",sub);
+
+
+    }
+    if(x>a);
+    {
+        mul=a*b;
+        printf("multiplication is %d",mul);
+
+
+    }
+
+    if(x<b);
+    {
+        dev=a/b;
+        printf("devision is %d",dev);
+        getch();
+    }
+
+
+}
